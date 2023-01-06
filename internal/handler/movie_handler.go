e1db8e437bed483ba3df2501b738fbdeda222787
pvcanh <pvcanh@tma.com.vn>
2023-01-06 17:03:01 +0700

    done service and

--- internal/handler/movie_handler.go
+++ internal/handler/movie_handler.go
@@ -1,1 +1,32 @@
 package handler
+
+import (
+	"github.com/gorilla/mux"
+	. "go-service/internal/service"
+	"net/http"
+)
+
+type MovieHandler struct {
+	servie MovieService
+}
+
+func NewMovieHandler(service MovieService) *MovieHandler {
+	return &MovieHandler{servie: service}
+}
+
+func (h *MovieHandler) All(w http.ResponseWriter, r *http.Request) {
+	res, err := h.servie.All(r.Context())
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	JSON(w, http.StatusOK, res)
+}
+
+func (h *MovieHandler) Load(w http.ResponseWriter, r http.Request) {
+	id := mux.Vars(r)["id"]
+	if len(id) == 0 {
+		http.Error(w, "Id cannot be empty", http.StatusBadRequest)
+		return
+	}
+}
--- internal/service/movie_service.go
+++ internal/service/movie_service.go
@@ -3,9 +3,12 @@ package service
 import (
 	"context"
 	"database/sql"
+	"fmt"
 	q "github.com/core-go/sql"
 	. "go-service/internal/filter"
 	. "go-service/internal/model"
+	"reflect"
+	"strings"
 )
 
 type MovieService interface {
@@ -13,7 +16,7 @@ type MovieService interface {
 	Load(ctx context.Context, id string) (*Movie, error)
 	Insert(ctx context.Context, movie *Movie) (int64, error)
 	Update(ctx context.Context, movie *Movie) (int64, error)
-	Patch(ctx context.Context, user map[string]interface{}) (int64, error)
+	Patch(ctx context.Context, movie map[string]interface{}) (int64, error)
 	Delete(ctx context.Context, id string) (int64, error)
 	Search(ctx context.Context, filter MovieFilter) (*ResultMovie, error)
 }
@@ -47,7 +50,7 @@ func (m *movieService) All(ctx context.Context) ([]Movie, error) {
 	return movies, nil
 }
 
-func (m movieService) Load(ctx context.Context, id string) (*Movie, error) {
+func (m *movieService) Load(ctx context.Context, id string) (*Movie, error) {
 	query := "select * from movies where id = ? limit 1"
 	rows, err := m.DB.QueryContext(ctx, query, id)
 	if err != nil {
@@ -66,7 +69,7 @@ func (m movieService) Load(ctx context.Context, id string) (*Movie, error) {
 
 }
 
-func (m movieService) Insert(ctx context.Context, movie *Movie) (int64, error) {
+func (m *movieService) Insert(ctx context.Context, movie *Movie) (int64, error) {
 	query := "insert into movies (id, name, watched) values (?,?.?)"
 	stmt, err := m.DB.Prepare(query)
 	if err != nil {
@@ -79,22 +82,111 @@ func (m movieService) Insert(ctx context.Context, movie *Movie) (int64, error) {
 	return res.RowsAffected()
 }
 
-func (m movieService) Update(ctx context.Context, movie *Movie) (int64, error) {
-	//TODO implement me
-	panic("implement me")
+func (m *movieService) Update(ctx context.Context, movie *Movie) (int64, error) {
+	query := "update movies set name = ?, watched = ?"
+	stmt, err := m.DB.Prepare(query)
+	if err != nil {
+		return -1, err
+	}
+	res, err1 := stmt.ExecContext(ctx, movie.Name, movie.Watched)
+	if err1 != nil {
+		return -1, err1
+	}
+	return res.RowsAffected()
 }
 
-func (m movieService) Patch(ctx context.Context, user map[string]interface{}) (int64, error) {
-	//TODO implement me
-	panic("implement me")
+func (m *movieService) Patch(ctx context.Context, movie map[string]interface{}) (int64, error) {
+	movieType := reflect.TypeOf(Movie{})
+	jsonColumnMap := q.MakeJsonColumnMap(movieType)
+	colMap := q.JSONToColumns(movie, jsonColumnMap)
+	keys, _ := q.FindPrimaryKeys(movieType)
+	query, agrs := q.BuildToPatch("movies", colMap, keys, q.BuildParam)
+	res, err := m.DB.ExecContext(ctx, query, agrs...)
+	if err != nil {
+		return -1, err
+	}
+	return res.RowsAffected()
 }
 
 func (m movieService) Delete(ctx context.Context, id string) (int64, error) {
-	//TODO implement me
-	panic("implement me")
+	query := "delete from movie where id = ?"
+	stmt, err := m.DB.Prepare(query)
+	if err != nil {
+		return -1, err
+	}
+	res, err := stmt.ExecContext(ctx, id)
+	if err != nil {
+		return -1, err
+	}
+	return res.RowsAffected()
 }
 
 func (m movieService) Search(ctx context.Context, filter MovieFilter) (*ResultMovie, error) {
-	//TODO implement me
-	panic("implement me")
+	query, param := BuildMovieQuery(filter, m.BuildParam)
+	rows, err := m.DB.QueryContext(ctx, query, param...)
+	if err != nil {
+		return nil, err
+	}
+	var movies []Movie
+	for rows.Next() {
+		movie := Movie{}
+		err := rows.Scan(&movie.Id, &movie.Name, &movie.Watched)
+		if err != nil {
+			return nil, err
+		}
+		movies = append(movies, movie)
+	}
+	query, params := BuildMovieCount(filter, m.BuildParam)
+	rows, err = m.DB.QueryContext(ctx, query, params...)
+	if err != nil {
+		return nil, err
+	}
+	var total int64
+	for rows.Next() {
+		err := rows.Scan(&total)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return &ResultMovie{List: movies, Total: total}, nil
+}
+
+func BuildMovieCount(filter MovieFilter, buildParam func(int) string) (string, []interface{}) {
+	query := "select count* from movie"
+	where, params := BuildMovieFilter(filter, buildParam)
+	if len(where) > 0 {
+		query = query + " where " + where
+	}
+	return query, params
+}
+
+func BuildMovieQuery(filter MovieFilter, buildParam func(int) string) (string, []interface{}) {
+	query := "select * from movies"
+	where, params := BuildMovieCount(filter, buildParam)
+	if len(where) > 0 {
+		query = query + " where " + where
+	}
+	return query, params
+}
+
+func BuildMovieFilter(filter MovieFilter, buildParam func(int) string) (string, []interface{}) {
+	var condition []string
+	var params []interface{}
+	i := 1
+	if len(filter.Id) > 0 {
+		params = append(params, filter.Id)
+		condition = append(condition, fmt.Sprint(`id = %s`, buildParam(i)))
+		i++
+	}
+	if len(filter.Name) > 0 {
+		q := "%" + filter.Name + "%"
+		params = append(params, q)
+		condition = append(condition, fmt.Sprintf(`name like %s`, buildParam(i)))
+		i++
+	}
+	if len(condition) > 0 {
+		return strings.Join(condition, " and "), params
+	} else {
+		return "", params
+	}
 }
