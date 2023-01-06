5904962e748a87ef3d3e0150f44402b452132bd3
CanhPhan <pvcanh.tma.com.vn>
2023-01-06 08:17:47 +0700

    not done service

--- internal/service/movie_service.go
+++ internal/service/movie_service.go
@@ -10,7 +10,7 @@ import (
 
 type MovieService interface {
 	All(ctx context.Context) ([]Movie, error)
-	Load(ctx context.Context, id string) (*User, error)
+	Load(ctx context.Context, id string) (*Movie, error)
 	Insert(ctx context.Context, movie *Movie) (int64, error)
 	Update(ctx context.Context, movie *Movie) (int64, error)
 	Patch(ctx context.Context, user map[string]interface{}) (int64, error)
@@ -28,19 +28,55 @@ func NewMovieService(db *sql.DB) MovieService {
 	return &movieService{DB: db, BuildParam: buildParam}
 }
 
-func (m movieService) All(ctx context.Context) ([]Movie, error) {
-	//TODO implement me
-	panic("implement me")
+func (m *movieService) All(ctx context.Context) ([]Movie, error) {
+	query := "select * from movies"
+	rows, err := m.DB.QueryContext(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var movies []Movie
+	for rows.Next() {
+		var movie Movie
+		err = rows.Scan(&movie.Id, &movie.Name, &movie.Watched)
+		if err != nil {
+			return nil, err
+		}
+		movies = append(movies, movie)
+	}
+	return movies, nil
 }
 
-func (m movieService) Load(ctx context.Context, id string) (*User, error) {
-	//TODO implement me
-	panic("implement me")
+func (m movieService) Load(ctx context.Context, id string) (*Movie, error) {
+	query := "select * from movies where id = ? limit 1"
+	rows, err := m.DB.QueryContext(ctx, query, id)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var movie Movie
+		err := rows.Scan(&movie.Id, &movie.Name, &movie.Watched)
+		if err != nil {
+			return nil, err
+		}
+		return &movie, nil
+	}
+	return nil, nil
+
 }
 
 func (m movieService) Insert(ctx context.Context, movie *Movie) (int64, error) {
-	//TODO implement me
-	panic("implement me")
+	query := "insert into movies (id, name, watched) values (?,?.?)"
+	stmt, err := m.DB.Prepare(query)
+	if err != nil {
+		return -1, err
+	}
+	res, err1 := stmt.ExecContext(ctx, movie.Id, movie.Name, movie.Watched)
+	if err1 != nil {
+		return -1, err1
+	}
+	return res.RowsAffected()
 }
 
 func (m movieService) Update(ctx context.Context, movie *Movie) (int64, error) {
